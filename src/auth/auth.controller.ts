70056f1831cfa304496c8a590840cdd890d68b76
sh1nn0on3 <sanogaku31@gmail.com>
2023-12-04 15:29:27 +0700

    docker

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-    // auth 
-    constructor(private authService : AuthService) {
+  // auth service is injected into the constructor
+  constructor(private authService: AuthService) {
+    authService.doSomeThing();
+  }
 
-    }
+  @Post('register')
+  register() {
+    return this.authService.register();
+  }
+
+  @Post('login')
+  login() {}
+
+  @Post('logout')
+  logout() {}
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  doSomeThing() {
+    console.log('Auth service does something');
+  }
+
+  register() {}
+
+  login() {}
+
+  logout() {}
+}
